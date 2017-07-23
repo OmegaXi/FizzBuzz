8c5a47b0d7f9438c34be4d281bc61a50032764db
Yestin <641010761@qq.com>
2017-07-23 19:01:26 +0800

    1

--- src/main/FizzBuzz.java
+++ src/main/FizzBuzz.java
@@ -4,7 +4,13 @@ package main;
  * Created by PANYE on 7/20/2017.
  */
 public class FizzBuzz {
-    public static String fizzBuzz(int number) {
-        return "Fizz";
-    }
+	public static String fizzBuzz(int number) {
+		if(number%3==0&&number%5!=0)
+			return "Fizz";
+		else if(number%3!=0&&number%5==0)
+			return "Buzz";
+		else if(number%3==0&number%5==0)
+			return "FizzBuzz";
+		else return String.valueOf(number);
+	}
 }
